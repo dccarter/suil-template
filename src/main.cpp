f5dec5b92261202ae61a21534e1d4c4285c21e3e
Carter Mbotho <lastcarter@gmail.com>
2021-01-15 23:48:53 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Mpho Mbotho on 2021-01-15.
+//
+#include <suil/base/logging.hpp>
+
+int main(int argc, char *argv[])
+{
+    sinfo("Hello World! This is suil world");
+    return 0;
+}
