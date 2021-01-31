9df5f65b8cd527ec0803d32600d2c2c7f694a0d5
Carter Mbotho <lastcarter@gmail.com>
2021-01-31 17:38:07 -0500

    Update project name to hello-world

--- 3rdParty.cmake
+++ 3rdParty.cmake
@@ -11,19 +11,3 @@ ExternalProject_Add(catch
         GIT_TAG "v2.13.1"
         CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${EP_INSTALL_DIR};-DCATCH_BUILD_EXAMPLES=OFF;-DCATCH_BUILD_TESTING=OFF;-DCATCH_INSTALL_DOCS=OFF;-DCATCH_INSTALL_HELPERS=OFF")
 set_target_properties(catch PROPERTIES EXCLUDE_FROM_ALL True)
-
-ExternalProject_Add(iod
-        PREFIX ${EP_PREFIX}/iod
-        GIT_REPOSITORY https://gitlab.com/sw-devel/thirdparty/iod.git
-        CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${EP_INSTALL_DIR}")
-set_target_properties(iod PROPERTIES EXCLUDE_FROM_ALL True)
-
-include_directories(${EP_INSTALL_DIR}/include)
-link_directories(${EP_INSTALL_DIR}/lib)
-set(CMAKE_MODULE_PATH
-        ${CMAKE_MODULE_PATH} ${EP_INSTALL_DIR}/share/cmake/Modules)
-
-add_custom_target(deps
-        DEPENDS catch iod)
-
-set_target_properties(deps PROPERTIES EXCLUDE_FROM_ALL True)
